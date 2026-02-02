9b9514b8b050dd45cc8b212cb616fa3f86201a2b
KeviN-XVII <kevinquartar@gmail.com>
2026-02-02 14:18:40 +0100

    Initial commit

---
+++ src/main/java/kevinquarta/s5l1BE/S5l1BeApplication.java
@@ -0,0 +1,13 @@
+package kevinquarta.s5l1BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5l1BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5l1BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kevinquarta/s5l1BE/S5l1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinquarta.s5l1BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5l1BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
